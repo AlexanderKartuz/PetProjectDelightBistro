8f40779ef0ba4f075a58f5cf82332a7eecd0abeb
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-05-19 21:14:17 +0300

    add js remove on html allfooditems

--- Net23Online/WebNet23Online/wwwroot/js/delight-bistro/allfoods.js
+++ Net23Online/WebNet23Online/wwwroot/js/delight-bistro/allfoods.js
@@ -1,9 +1,17 @@
 $(document).ready(function () {
   $("div.food-item").click(function () {
     const self = $(this);
-
     self.toggleClass("active");
 
-    //const atLeastOneItemForRemove = $("div.food-item.active").length > 0;
+    const atLeastOneItemIsSelected = $("div.food-item.active").length > 0;
+
+    if (atLeastOneItemIsSelected) {
+      $(".remove-food-item").removeAttr("disabled");
+    } else {
+      $(".remove-food-item").attr("disabled");
+    }
+  });
+  $(".remove-food-item").click(function () {
+    $("div.food-item.active").remove();
   });
 });
