4f162556165d41c141fccf374aa6a7330e3f0853
Nikita Zholnerovich <nikita.zholnerovich21@gmail.com>
2026-05-20 17:44:56 +0300

    Add delete game scripti from catalog (only from DOM variant)

---
+++ Net23Online/WebNet23Online/wwwroot/js/steam/catalog-delete-game.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('article.card').click(function () {
+        const self = $(this);
+        self.toggleClass('active');
+        const atLeastOneItemForRemove = $('article.card.active').length > 0
+
+        if (atLeastOneItemForRemove) {
+            $('.form-actions .remove-game-card').removeAttr('disabled');
+        } else {
+            $('.form-actions .remove-game-card').attr('disabled', 'disabled');
+        }
+    })
+
+    $('.form-actions .remove-game-card').click(function () {
+        $('article.card.active').remove();
+    });
+});
