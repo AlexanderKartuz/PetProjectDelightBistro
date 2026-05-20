cc3656cc12f10da3a6184672eb2e7a2987949b99
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-05-20 23:59:40 +0300

    add ajax delete food items

---
+++ Net23Online/WebNet23Online/Controllers/ApiControllers/DelightBistroController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebNet23Online.Data.Repositories.Interfaces.DelightBistro;
+
+namespace WebNet23Online.Controllers.ApiControllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DelightBistroController : ControllerBase
+    {
+        private IFoodItemRepository _foodItemRepository;
+        public DelightBistroController(IFoodItemRepository foodItemRepository)
+        {
+            _foodItemRepository = foodItemRepository;
+        }
+
+        public bool Delete([FromQuery] List<int> ids)
+        {
+            _foodItemRepository.Delete(ids);
+            return true;
+        }
+    }
+}
