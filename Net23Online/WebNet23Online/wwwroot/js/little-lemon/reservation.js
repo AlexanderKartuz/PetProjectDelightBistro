f5c10ba9fd527561632eb4a996d40a694e1c76ec
Osama-arch <Osamarchitectinfo@gmail.com>
2026-05-20 09:48:39 +0300

    Add /remove photo from attached file in reservation

---
+++ Net23Online/WebNet23Online/wwwroot/js/little-lemon/reservation.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    const imagePreview = $(".image-preview_image");
+    const imagePreviewWrapper = $(".image-preview_image__wrapper");
+    const imagePreviewBtn = $(".image-preview_btn");
+    const imagePreviewContainer = $(".image-preview_container");
+    $("#DessertReferencePhoto").on("change", function () {
+        const file = this.files[0];
+        if (file) {
+            imagePreviewContainer.removeClass("hide");
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                imagePreview.attr("src", e.target.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+    imagePreviewBtn.on("click", function () {
+        imagePreviewContainer.addClass("hide");
+    });
+});
