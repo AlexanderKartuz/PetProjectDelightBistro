b40cd62945125515d0a65a1e7bdf410c1e6ed7cd
Osama-arch <Osamarchitectinfo@gmail.com>
2026-05-20 10:15:13 +0300

    fix dessert Reference Photo behavior

--- Net23Online/WebNet23Online/wwwroot/js/little-lemon/reservation.js
+++ Net23Online/WebNet23Online/wwwroot/js/little-lemon/reservation.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
     const imagePreview = $(".image-preview_image");
-    const imagePreviewWrapper = $(".image-preview_image__wrapper");
     const imagePreviewBtn = $(".image-preview_btn");
     const imagePreviewContainer = $(".image-preview_container");
-    $("#DessertReferencePhoto").on("change", function () {
+    const dessertReferencePhoto = $("#DessertReferencePhoto");
+    $(dessertReferencePhoto).on("change", function () {
         const file = this.files[0];
         if (file) {
             imagePreviewContainer.removeClass("hide");
             const reader = new FileReader();
             reader.onload = function () {
-                imagePreview.attr("src",reader.result);
+                imagePreview.attr("src", reader.result);
             };
             reader.readAsDataURL(file);
         }
     });
     imagePreviewBtn.on("click", function () {
         imagePreviewContainer.addClass("hide");
+        dessertReferencePhoto.val("");
     });
 });
