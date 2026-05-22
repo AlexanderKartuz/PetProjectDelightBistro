159b97a4ab7c939fb0891720edd63c583954945e
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-05-22 21:38:24 +0300

    add selected foodItemName

--- Net23Online/WebNet23Online/wwwroot/js/delight-bistro/buy-button.js
+++ Net23Online/WebNet23Online/wwwroot/js/delight-bistro/buy-button.js
@@ -1,34 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
   const buyButtons = document.querySelectorAll('.buy-button');
   const counterDisplay = document.querySelector('.counter-display');
+  const orderBox = document.querySelector('.order-box');
+  const orderList = document.querySelector('#orderList');
 
   buyButtons.forEach((button) => {
     button.addEventListener('click', function () {
       const self = this;
       self.classList.toggle('choose-to-buy');
 
-      const choosenFoodIds = getChosenIds();
-
-      console.log('выбранные id:', choosenFoodIds);
-      counterDisplay.textContent = choosenFoodIds.length;
+      updateOrderBox();
     });
   });
-  // список всех кнопки которые выбраны
-  // const foodIds = choosenButtons.querySelectorAll('data-food-item-id');
 
-  function getChosenIds() {
+  function updateOrderBox() {
+    const chossenItems = getChosenItems();
+    counterDisplay.textContent = chossenItems.length;
+    const oldItemNames = orderList.querySelectorAll('.order-food-name');
+    oldItemNames.forEach((item) => item.remove());
+
+    chossenItems.forEach((item) => {
+      const li = document.createElement('li');
+      li.className = 'order-food-name';
+      li.textContent = item.name;
+      orderList.appendChild(li);
+    });
+
+    if (chossenItems.length > 0) {
+      orderBox.classList.remove('hidden');
+    } else {
+      orderBox.classList.add('hidden');
+    }
+    console.log(chossenItems);
+  }
+
+  function getChosenItems() {
     const choosenButtons = document.querySelectorAll(
       '.buy-button.choose-to-buy',
     );
-
-    const ids = [];
+    const orderFoodItems = [];
 
     choosenButtons.forEach((button) => {
-      // получаем id foodItem по атрибуту кнопки
       const id = button.dataset.foodItemId;
-      ids.push(parseInt(id));
+      const foodItem = button.closest('.food-item');
+      const foodItemName = foodItem.querySelector('.food-name');
+      const name = foodItemName ? foodItemName.textContent.trim() : 'No name';
+
+      orderFoodItems.push({ id, name });
     });
 
-    return ids;
+    return orderFoodItems;
   }
 });
