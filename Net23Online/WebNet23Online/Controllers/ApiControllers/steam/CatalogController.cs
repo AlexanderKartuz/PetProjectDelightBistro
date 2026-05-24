e7a477f7dbbab23d55c720c58557d70acd6a998a
Nikita Zholnerovich <nikita.zholnerovich21@gmail.com>
2026-05-24 13:54:54 +0300

    Add ajax request for deleting games

---
+++ Net23Online/WebNet23Online/Controllers/ApiControllers/steam/CatalogController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebNet23Online.Controllers.CustomAuthAttribute;
+using WebNet23Online.Data.Repositories.Interfaces.Steam;
+using WebNet23Online.Services.Interfaces;
+
+namespace WebNet23Online.Controllers.ApiControllers.steam
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IGameRepository _gameRepository;
+        private readonly IAuthService _authService;
+
+        public CatalogController(IGameRepository gameRepository, IAuthService authService)
+        {
+            _gameRepository = gameRepository;
+            _authService = authService;
+        }
+
+        [IsAdminApi]
+        public bool Delete([FromQuery] List<int> gameIds)
+        {
+            _gameRepository.Delete(gameIds);
+            return true;
+        }
+    }
+}
