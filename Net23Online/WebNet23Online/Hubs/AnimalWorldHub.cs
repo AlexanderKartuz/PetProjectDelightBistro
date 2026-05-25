eb6df694ef9b1c117165883891573dc5e9f26031
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-05-25 22:41:39 +0300

    Start work with SignalR.

---
+++ Net23Online/WebNet23Online/Hubs/AnimalWorldHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebNet23Online.Hubs
+{
+    public class AnimalWorldHub : Hub<IAnimalWorldHub>
+    {
+    }
+
+    public interface IAnimalWorldHub
+    {
+        Task NewZooCreated(string zooName);
+    }
+}
