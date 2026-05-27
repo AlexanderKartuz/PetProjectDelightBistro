9f30000973e963ef4ed76978967ebda8dfa52b03
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-05-27 21:35:51 +0300

    Add minimal API

---
+++ Net23Online/MovieMinimalApi/DbStuff/MiniDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieMinimalApi.DbStuff
+{
+    public class MiniDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        public MiniDbContext(DbContextOptions<MiniDbContext> options) : base(options) { }
+    }
+}
