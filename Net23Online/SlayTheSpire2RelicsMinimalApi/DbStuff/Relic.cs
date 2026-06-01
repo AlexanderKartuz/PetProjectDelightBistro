a741d2ece0020e8c1f6e766a6a008aea363a5edb
Zalman <catwithshotgunster34@gmail.com>
2026-06-01 18:42:11 +0300

    MinimalApi for SlayTheSpire2 + add relics page

---
+++ Net23Online/SlayTheSpire2RelicsMinimalApi/DbStuff/Relic.cs
@@ -0,0 +1,10 @@
+﻿namespace SlayTheSpire2RelicsMinimalApi.DbStuff
+{
+    public class Relic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UrlImage { get; set; }
+        public string Rarity { get; set; }
+    }
+}
