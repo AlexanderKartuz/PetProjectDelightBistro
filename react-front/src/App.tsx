c166cbf517df591639d7fe90f0d9af5d21e3bbfb
Geliusan3 <stasx2011@gmail.com>
2026-06-08 16:52:00 +0300

    E2E Test + React

--- react-front/src/App.tsx
+++ react-front/src/App.tsx
@@ -4,6 +4,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { Counter } from './components/counter'
 import { MovieList } from './components/movie-list'
+import { RockQuotesContainer } from './components/rock-quotes-container';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         </div>
         <Counter />
         <MovieList />
+        <RockQuotesContainer />
       </section>
 
       <div className="ticks"></div>
