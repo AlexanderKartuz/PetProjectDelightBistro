da7a4bc69f473c88473363487ec5af85f2d6dc08
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-06-08 09:09:36 +0300

    First version.

---
+++ react-front/src/components/animal-fact-card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import type { AnimalFact } from '../types/animal-fact'
+
+interface AnimalFactCardProps {
+  fact: AnimalFact
+}
+
+export const AnimalFactCard = function ({ fact }: AnimalFactCardProps) {
+  return (
+    <div className="animal fact-item">
+      <span className="fact-animal-type">{fact.animalSpeciesName}</span>
+      <p className="comment-text">{fact.text}</p>
+    </div>
+  )
+}
