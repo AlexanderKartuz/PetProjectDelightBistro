cc6a90dcc756583e1357be1793cff8a2f9012e28
Pavel Lvou <pavel.lvou@hiqo-solutions.com>
2026-06-17 21:22:23 +0300

    Add async

---
+++ Net23Online/MultyThreadExample/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using MultyThreadExample;
+
+var user = new User();
+var payload = new Payload();
+payload.counter = 0;
+
+var taskAAA = new Task(() =>
+{
+    user.Do(payload);
+});
+
+var taskBBB = new Task(() =>
+{
+    user.Do(payload);
+});
+
+taskAAA.Start();
+taskBBB.Start();
+
+
+Thread.Sleep(1000 * 2000);
---
+++ Net23Online/WebNet23Online/Models/DTOs/CatDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebNet23Online.Models.DTOs
+{
+    public class CatDto
+    {
+        public string id { get; set; }
+        public List<string> tags { get; set; }
+        public string mimetype { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+}
