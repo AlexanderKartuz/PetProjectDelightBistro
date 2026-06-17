173b4dea19ebfd7fc463303b12b4c2c3a71ed4e3
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-06-17 16:58:30 +0300

    Delete Net23Online/DelightBistroMinimalApi/Middlewares/Cache/Options/IDistributedCacheOptions.cs

--- Net23Online/DelightBistroMinimalApi/Middlewares/Cache/Options/IDistributedCacheOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DelightBistroMinimalApi.Middlewares.Cache.Options
-{
-    public interface IDistributedCacheOptions
-    {
-        int AbsoluteMinutes { get; set; }
-        int SlidingMinutes { get; set; }
-    }
-}
