e4ac63d782e869aa989cecf013dfe3a28e9f60e2
MikalaiTarasenka <ntarasenko92@gmail.com>
2026-06-23 11:57:28 +0300

    Finally it appeared!!! Next debugging and testing.

--- Net23Online/WebNet23Online/wwwroot/js/animal-world/promotions.js
+++ Net23Online/WebNet23Online/wwwroot/js/animal-world/promotions.js
@@ -1,12 +1,13 @@
 ﻿$(document).ready(function () {
-    const url = `https://localhost:7284/my-hub/animal-world`;
+    const url = `https://localhost:7284/my-hub/animal-world-promotions`;
     const hub = new signalR.HubConnectionBuilder().withUrl(url).build();
 
-    hub.on('NewAnimalInZooAppeared', function (zooName, animalSpeciesName) {
+    hub.on('ZoosPromotions', function (text) {
+        console.log("Сработал метод!", text);
         const notification = $('<div>').addClass('zoo-notification');
-        const title = $('<div>').addClass('zoo-notification-title').text('Пополнение в зоопарке!');
+        const title = $('<div>').addClass('zoo-notification-title').text('Акция!');
 
-        const bodyText = `В зоопарке "${zooName}" появился новый вид животного — ${animalSpeciesName}!`;
+        const bodyText = text;
         const body = $('<div>').addClass('zoo-notification-body').text(bodyText);
 
         notification.append(title).append(body);
