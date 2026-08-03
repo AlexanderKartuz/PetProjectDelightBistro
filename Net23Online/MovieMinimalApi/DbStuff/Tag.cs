97c11f51484c34f1021655c77775598eb217db6c
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-08-03 22:21:17 +0300

    remove non owner code

--- Net23Online/MovieMinimalApi/DbStuff/Tag.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieMinimalApi.DbStuff
-{
-    public class Tag
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Movie> Movies { get; set; } = new List<Movie>();
-    }
-}
