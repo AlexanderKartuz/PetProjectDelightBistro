979d5c4c5dcf07b4421cc819a5d3e7014125a606
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-08-07 21:21:58 +0300

    update ApiUrl in React

--- react-delight-bistro-app/src/services/drinks-service.ts
+++ react-delight-bistro-app/src/services/drinks-service.ts
@@ -1,16 +1,16 @@
 import type { CreateDrinkPayload, Drink } from '../types/drinks.js';
 
 const API_BASE = 'https://localhost:7090';
-const API_GET_TEAS = '/GetTeas';
-const API_POST_CREATE_TEA = '/CreateTea';
+const API_GET_DRINKS = '/GetDrinks';
+const API_POST_CREATE_DRINK = '/CreateDrink';
 const API_DELETE = '/DeleteDrink';
 const API_CHANGE = '/ChangeDrink';
-const API_GET_TEA = '/GetTea';
+const API_GET_DRINK = '/GetDrink';
 
 // GetTea/{id}
 
 export async function getDrinks(): Promise<Drink[]> {
-  const response = await fetch(`${API_BASE}${API_GET_TEAS}`);
+  const response = await fetch(`${API_BASE}${API_GET_DRINKS}`);
 
   if (!response.ok) {
     throw new Error(`Ошибка запроса ${response.status}`);
@@ -21,7 +21,7 @@ export async function getDrinks(): Promise<Drink[]> {
 }
 
 export async function createDrink(payload: CreateDrinkPayload): Promise<Drink> {
-  const response = await fetch(`${API_BASE}${API_POST_CREATE_TEA}`, {
+  const response = await fetch(`${API_BASE}${API_POST_CREATE_DRINK}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload),
@@ -69,7 +69,7 @@ export async function changeDrink(
 }
 
 export async function getDrink(id: number): Promise<Drink> {
-  const response = await fetch(`${API_BASE}${API_GET_TEA}/${id}`, {
+  const response = await fetch(`${API_BASE}${API_GET_DRINK}/${id}`, {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
