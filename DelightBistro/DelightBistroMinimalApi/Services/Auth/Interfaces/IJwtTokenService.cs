1fc2c2ead818f6bd2c5950343bbc5bafbe8378bd
AlexanderKartuz <113421321+AlexanderKartuz@users.noreply.github.com>
2026-08-15 22:05:31 +0300

    add jwt to minimal api

---
+++ DelightBistro/DelightBistroMinimalApi/Services/Auth/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using DelightBistroMvc.Data.Models;
+
+namespace DelightBistroMinimalApi.Services.Auth.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        string CreateToken(UserData user);
+    }
+}
---
+++ DelightBistro/DelightBistroMinimalApi/Services/Auth/JwtTokenService.cs
@@ -0,0 +1,43 @@
+﻿using DelightBistroMinimalApi.Services.Auth.Interfaces;
+using DelightBistroMinimalApi.Services.Auth.Options;
+using DelightBistroMvc.Data.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DelightBistroMinimalApi.Services.Auth
+{
+    public class JwtTokenService : IJwtTokenService
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenService(IOptions<JwtOptions> options)
+        {
+            _jwtOptions = options.Value;
+        }
+
+        public string CreateToken(UserData user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new (ClaimTypes.Role, user.Role.ToString()),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtOptions.Issuer,
+                audience: _jwtOptions.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtOptions.ExpireMinutes),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
